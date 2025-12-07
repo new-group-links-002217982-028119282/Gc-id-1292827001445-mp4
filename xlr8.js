834fc52547132d0c8563dc543a3023ff7b689b22
new-group-links-002217982-028119282 <jhayreinzuella@gmail.com>
2025-12-07 15:45:40 +0800

    Create xlr8.js

---
+++ xlr8.js
@@ -0,0 +1,68 @@
+ const links = [
+ 
+ "https://s.shopee.ph/50RXnlprDH",
+ "https://s.shopee.ph/7V8smPb0H3",
+ "https://s.shopee.ph/50RXnqNRIV",
+ "https://s.shopee.ph/BMI2zNQta",
+ "https://s.shopee.ph/8fKqAeCOz7",
+ "https://s.shopee.ph/8fKqAfy3Eg",
+ "https://s.shopee.ph/2g3d1fy9fY",
+ "https://s.shopee.ph/Vz8RisfgA",
+ "https://s.shopee.ph/2B7MQo3jAd",
+ "https://s.shopee.ph/40Z0cCDSi7",
+ 
+ "https://s.shopee.ph/2g3d1n8Whu",
+ "https://s.shopee.ph/7V8smhOv70",
+ "https://s.shopee.ph/2qN3E9B0ki",
+ "https://s.shopee.ph/LfiFZxvbo",
+ "https://s.shopee.ph/gIYeFCq8r",
+ "https://s.shopee.ph/AUmUMLF9Hp",
+ "https://s.shopee.ph/4q87byQMm8",
+ "https://s.shopee.ph/30gTQdJotV",
+ "https://s.shopee.ph/3Vck1aTgm5",
+ 
+ 
+ "https://s.shopee.ph/4LBr1A8Bez",
+ "https://s.shopee.ph/6ptBzn6BDo",
+ "https://s.shopee.ph/6AdVCav7iF",
+ "https://s.shopee.ph/6KwvOwFkSQ",
+ "https://s.shopee.ph/5VNoPR4ILz",
+ "https://s.shopee.ph/9fDNN7adFJ",
+ "https://s.shopee.ph/6ptBzwG9km",
+ "https://s.shopee.ph/8zxgZwpdeK",
+ "https://s.shopee.ph/9AH6mHVyjX",
+ "https://s.shopee.ph/5L4ODKMpwu",
+ 
+ 
+ "https://s.shopee.ph/4fohQ9skrI",
+ "https://s.shopee.ph/9KaWylVv2v",
+ "https://s.shopee.ph/9pWnZi2Btb",
+ "https://s.shopee.ph/2VkCqFuRjU",
+ "https://s.shopee.ph/4q87cZG5wt",
+ "https://s.shopee.ph/4LBr1fqJLW",
+ "https://s.shopee.ph/5Aky1Er8v2",
+ "https://s.shopee.ph/4AsQpQpXHs",
+ "https://s.shopee.ph/8AOZaoQuIw",
+ "https://s.shopee.ph/1qUW3D146U",
+ 
+ "https://s.shopee.ph/3fwAEcO4ll",
+ "https://s.shopee.ph/5L4ODiZ4Wn",
+ "https://s.shopee.ph/20nwFcKUFE",
+ "https://s.shopee.ph/2VkCqaCHgo",
+ "https://s.shopee.ph/9AH6msPh7E",
+ "https://s.shopee.ph/2LQmeLtVSl",
+ "https://s.shopee.ph/2LQmeNSqm2",
+ "https://s.shopee.ph/1VrfesiPit",
+ "https://s.shopee.ph/4q87d5pdP3",
+ "https://s.shopee.ph/5L4OE2mNBb",
+ 
+ "https://s.shopee.ph/1qUX0ujxNo",
+ ];
+ 
+ const randomLink = links[Math.floor(Math.random() * links.length)];
+ 
+ document.write(
+ `<meta http-equiv="refresh" content="0;url=${randomLink}">`
+ );
+
+ 
